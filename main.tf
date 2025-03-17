223c8a992dbff12cdec530afbaf3ec2b531568a8
azureuser180 <142494675+azureuser180@users.noreply.github.com>
2025-03-17 16:17:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "my_instance" {
   ami           = "ami-0e1bed4f06a3b463d"
   instance_type = "t2.micro"
-  key_name      = "test"  # Replace with your actual key pair name
+  key_name      = "test"  # Replace with your actual key pair name and it edit
   tags = {
     Name = "TerraformInstance"
   }
