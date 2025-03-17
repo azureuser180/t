f8981a508cbd31dc04acadbc0b5cd95c8eba42f1
Ubuntu <ubuntu@ip-172-31-20-2.ec2.internal>
2025-03-17 08:59:46 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+resource "aws_instance" "my_instance" {
+  ami           = "ami-0e1bed4f06a3b463d"
+  instance_type = "t2.micro"
+  key_name      = "test"  # Replace with your actual key pair name
+  tags = {
+    Name = "TerraformInstance"
+  }
+}
