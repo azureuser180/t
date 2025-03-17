e9338aac0f52cb242b4ec9ab31b85f5f29596a0c
azureuser180 <142494675+azureuser180@users.noreply.github.com>
2025-03-17 16:22:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "my_instance" {
   ami           = "ami-0e1bed4f06a3b463d"
   instance_type = "t2.micro"
-  key_name      = "test"  # Replace with your actual key pair name and it edit
+  key_name      = "test"  # Replace with your actual key pair ,i just wanted to change web hook by changing line here
   tags = {
     Name = "TerraformInstance"
   }
