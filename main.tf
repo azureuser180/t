218021bfe8d7c04ed75156c4a9c55cfe4ec62780
Ubuntu <ubuntu@ip-172-31-17-217.ec2.internal>
2025-03-17 11:20:08 +0000

    change 7

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "my_instance" {
   ami           = "ami-0e1bed4f06a3b463d"
   instance_type = "t2.micro"
-  key_name      = "test"  # Replace with your actual key pair , ok tested webhook chnage noe
+  key_name      = "test"  # Replace with your actual key pair , ok tested webhook change 7
   tags = {
     Name = "TerraformInstance"
   }
